cfaf46a3058c5ea44e5a6b8aff446d190979da0e
Гость <Guest@iMac-Mac005-6.local>
2022-04-21 13:47:14 +0900

    first commit

---
+++ xdx/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  xdx
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
